a909ef32b2435e985a5a2fcc934bd2432b0f1236
decathlon-kenya <contact.kenya@decathlon.com>
2025-08-10 21:57:54 +0300

    Update pages.js

--- cms/pages.js
+++ cms/pages.js
@@ -1,21 +1,4 @@
-function toggleDelivery(menuId, button) {
-    const menu = document.getElementById(menuId);
-    const isOpen = menu.style.maxHeight;
-
-    // Close all other menus
-    document.querySelectorAll('.delivery-menu').forEach((otherMenu) => {
-        if (otherMenu !== menu) {
-            otherMenu.style.maxHeight = null;
-            otherMenu.previousElementSibling.querySelector('i').className = 'ri-add-line';
-        }
-    });
-
-    // Toggle current menu
-    if (isOpen) {
-        menu.style.maxHeight = null;
-        button.querySelector('i').className = 'ri-add-line';
-    } else {
-        menu.style.maxHeight = menu.scrollHeight + 'px';
-        button.querySelector('i').className = 'ri-subtract-line';
-    }
+function toggleSection(header) {
+    const section = header.parentElement;
+    section.classList.toggle("pages-service-open");
 }
