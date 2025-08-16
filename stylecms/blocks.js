1846c18c2fcf091696f342c9cde7f1a84fee97b0
decathlon-kenya <contact.kenya@decathlon.com>
2025-08-16 19:51:20 +0300

    Update blocks.js

--- stylecms/blocks.js
+++ stylecms/blocks.js
@@ -1,18 +1,18 @@
-const allSportsSwiper = new Swiper('.all-sports-swiper-container', {
+const blockskeSwiper = new Swiper('.blockske-swiper-container', {
   loop: false,
   slidesPerView: 1.85,
   spaceBetween: 24,
   slidesPerGroup: 2,
   breakpoints: {
-     640: {
-        slidesPerView: 1.85,
-     },
-     1024: {
-        slidesPerView: 5,
-     }
+    640: {
+      slidesPerView: 1.85,
+    },
+    1024: {
+      slidesPerView: 5,
+    }
   },
   navigation: {
-     nextEl: '.all-sports-button-next',
-     prevEl: '.all-sports-button-prev',
+    nextEl: '.blockske-button-next',
+    prevEl: '.blockske-button-prev',
   },
-}); 
+});
